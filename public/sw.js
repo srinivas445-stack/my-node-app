5e9077b1c2b2c67971a693be6618497a88587763
srinivas445-stack <srinivasulupurini124@gmail.com>
2025-09-22 09:11:24 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,20 @@
-self.addEventListener('install', event => {
-  console.log('Service Worker installing.');
-  self.skipWaiting();
+self.addEventListener('install', (event) => {
+  console.log('Service Worker: Installed');
+  event.waitUntil(
+    caches.open('asset-qr-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/manifest.json',
+        '/icon-192.png'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('activate', event => {
-  console.log('Service Worker activated.');
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
